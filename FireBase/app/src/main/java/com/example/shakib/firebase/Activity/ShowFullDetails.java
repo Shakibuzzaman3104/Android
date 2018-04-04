8bd69da9cc14e568ba5e84825c8ea98f88df6579
shakibuzzaman3104 <shakibuzzaman3104@gmail.com>
2018-04-04 13:03:47 +0600

    Added inflater

---
+++ FireBase/app/src/main/java/com/example/shakib/firebase/Activity/ShowFullDetails.java
@@ -0,0 +1,45 @@
+package com.example.shakib.firebase.Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.shakib.firebase.R;
+
+
+public class ShowFullDetails extends Activity {
+
+    private TextView name,age,bg,email;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_user_details);
+
+        Intent intent = getIntent();
+
+        String Name = intent.getStringExtra("Name");
+        Toast.makeText(this, ""+Name, Toast.LENGTH_SHORT).show();
+        String AGE = intent.getStringExtra("Age");
+        Toast.makeText(this, ""+AGE, Toast.LENGTH_SHORT).show();
+        String Blood = intent.getStringExtra("Blood_Group");
+        String Mail = intent.getStringExtra("Email");
+
+        name=(TextView)findViewById(R.id.uname);
+        age=(TextView)findViewById(R.id.uage);
+        bg=(TextView)findViewById(R.id.uBG);
+        email=(TextView)findViewById(R.id.uEmail);
+
+
+        name.setText("Name : "+Name);
+        age.setText("Age : "+AGE);
+        bg.setText("Blood Group : "+Blood);
+        email.setText("Email : "+Mail);
+
+    }
+}
--- FireBase/app/src/main/java/com/example/shakib/firebase/Helpers/MyAdapter.java
+++ FireBase/app/src/main/java/com/example/shakib/firebase/Helpers/MyAdapter.java
@@ -1,12 +1,16 @@
 package com.example.shakib.firebase.Helpers;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.shakib.firebase.Activity.ShowFullDetails;
 import com.example.shakib.firebase.Model.Users;
 import com.example.shakib.firebase.R;
 
@@ -29,9 +33,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
             super(itemView);
 
             FirstName=itemView.findViewById(R.id.textView);
-            Age=itemView.findViewById(R.id.textView3);
+            /*Age=itemView.findViewById(R.id.textView3);
             Blood_Group=itemView.findViewById(R.id.textView4);
-            Email=itemView.findViewById(R.id.textView5);
+            Email=itemView.findViewById(R.id.textView5);*/
         }
     }
 
@@ -45,15 +49,28 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
 
-        String Name =arUsers.get(position).getFirst_Name()+" "+arUsers.get(position).getLast_Name();
+        final String Name =arUsers.get(position).getFirst_Name()+" "+arUsers.get(position).getLast_Name();
 
         holder.FirstName.setText(Name);
       //  holder.LastName.setText(arUsers.get(position).getLast_Name());
-        holder.Age.setText(arUsers.get(position).getAge());
+       /* holder.Age.setText(arUsers.get(position).getAge());
         holder.Blood_Group.setText(arUsers.get(position).getBG());
-        holder.Email.setText(arUsers.get(position).getEmail());
+        holder.Email.setText(arUsers.get(position).getEmail());*/
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i=new Intent(mContext,ShowFullDetails.class);
+                i.putExtra("Name", ""+Name.toString());
+                i.putExtra("Age", ""+arUsers.get(position).getAge().toString());
+                i.putExtra("Blood_Group", ""+arUsers.get(position).getBG().toString());
+                i.putExtra("Email", ""+arUsers.get(position).getEmail().toString());
+                mContext.startActivity(i);
+            }
+        });
     }
 
     @Override
